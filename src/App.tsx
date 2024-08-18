bdd56ff6a49e41d87f081643cc19e2045d98061c
fitronansori <fitronansori7@gmail.com>
2024-08-18 11:43:16 +0800

    chore: Add Hero component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Hero from "./components/layouts/Hero";
 import Navbar from "./components/layouts/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 };
