c2e9b867f8dad091b7b5d6c8d74302359ab0819d
fitronansori <fitronansori7@gmail.com>
2024-08-21 08:50:54 +0800

    feat: Add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Articles from "./components/layouts/Articles";
+import Footer from "./components/layouts/Footer";
 import Hero from "./components/layouts/Hero";
 import Leverage from "./components/layouts/Leverage";
 import Navbar from "./components/layouts/Navbar";
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <Leverage />
       <Articles />
+      <Footer />
     </>
   );
 };
